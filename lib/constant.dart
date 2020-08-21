63b1a626cca880b17e0299e99236eaf470606a1d
chairielazizi <azizihr2000@gmail.com>
2020-08-21 08:49:36 +0800

    declare the constants used and make the top container widget

---
+++ lib/constant.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+// colors
+const kBackgroundColor = Color(0xfffefefe);
+const kTitleColor = Color(0xff303030);
+const kBodyTextColor = Color(0xff4b4b4b);
+const kTextLightColor = Color(0xff959595);
+const kInfectedColor = Color(0xffff8748);
+const kDeathColor = Color(0xffff4848);
+const kRecoverColor = Color(0xff36c12c);
+const kPrimaryColor = Color(0xff3382cc);
+final kShadowColor = Color(0xffb7b7b7).withOpacity(.16);
+final kActiveShadowColor = Color(0xff4056c6).withOpacity(.15);
+
+// text styles
+const kHeadingTextStyle = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.w600,
+);
+
+const kSubTextStyle = TextStyle(
+  fontSize: 16,
+  color: kTextLightColor,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 18,
+  color: kTitleColor,
+  fontWeight: FontWeight.bold,
+);
